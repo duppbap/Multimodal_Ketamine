a59bd00560f3a4e2f992107b16f1e7cf82b38d9e
Wooseong <wooseong@caltech.edu>
2025-03-20 17:35:48 -0700

    memory 20 for rat 14

--- Core/mmKetamine_main.m
+++ Core/mmKetamine_main.m
@@ -37,7 +37,7 @@ idx = sample_idx.(sampleName);
 filename =[sampleName '.scan'];
 
 %update data path
-data_path = [data_path '/' allRats{3} '/figures'];
+data_path = [data_path '/' allRats{3} '/figures_m20'];
 
 %% preprocess
 mat4D = h5read(filename, '/Data');
